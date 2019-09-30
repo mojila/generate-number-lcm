8fe07872061a9f7cc10781a26b795706e0e742e6
mojila <mojila@gitlab.com>
2019-09-30 19:51:41 +0700

    fix

--- main.m
+++ main.m
@@ -1,26 +1,4 @@
-disp("Tugas 3 nomor 1");
-
-xs = 1;
-x = [];
-length = 100;
-
-function generated_number = lcm(xs1)
-    a = 3;
-    c = 15;
-    m = 2571;
-    
-    generated_number = rem((a * xs1 + c), m);
-    return;
-end
-
-for i = 1:length
-    if i == 1
-        x = lcm(xs);
-    else
-        x = [x lcm(x(1,i-1))];
-    end
-end
-disp("Tugas 3 nomor 1");
+disp("Bab 3 nomor 1");
 
 xs = 1;
 x = [];
