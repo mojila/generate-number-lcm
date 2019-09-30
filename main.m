526a8d62beb0c47b12632f1c035087ea8fbd9f03
mojila <mojila@gitlab.com>
2019-09-30 19:46:18 +0700

    add for matlab

---
+++ main.m
@@ -0,0 +1,47 @@
+disp("Tugas 3 nomor 1");
+
+xs = 1;
+x = [];
+length = 100;
+
+function generated_number = lcm(xs1)
+    a = 3;
+    c = 15;
+    m = 2571;
+    
+    generated_number = rem((a * xs1 + c), m);
+    return;
+end
+
+for i = 1:length
+    if i == 1
+        x = lcm(xs);
+    else
+        x = [x lcm(x(1,i-1))];
+    end
+end
+disp("Tugas 3 nomor 1");
+
+xs = 1;
+x = [];
+length = 100;
+
+function generated_number = lcm(xs1)
+    a = 3;
+    c = 15;
+    m = 2571;
+    
+    generated_number = rem((a * xs1 + c), m);
+    return;
+end
+
+for i = 1:length
+    if i == 1
+        x = lcm(xs);
+    else
+        x = [x lcm(x(1,i-1))];
+    end
+end
+
+disp("Hasil generate 100 angka tidak sama");
+disp(x);
